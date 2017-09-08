1fd984f5f48028ac4a21138d35fd572225d474e2
xenobarbital <greencross.am@gmail.com>
2017-09-08 12:19:27 +0400

    Shop edit works

--- app/routes/shop/edit.js
+++ app/routes/shop/edit.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model(params) {
+        return this.get('store').findRecord('shop', params.shop_id);
+    },
+
+    actions: {
+        saveShop(shop) {
+            shop.save().then(() => this.transitionTo('index'));
+        },
+
+        willTransition(transition) {
+            let model = this.controller.get('model');
+
+            if (model.get('hasDirtyAttributes')) {
+                let confirmation = confirm("Haven't saved yet. Leave this form?");
+
+                if (confirmation) {
+                    model.rollbackAttributes();
+                } else {
+                    transition.abort();
+                }
+            }
+        }
+    }
 });
